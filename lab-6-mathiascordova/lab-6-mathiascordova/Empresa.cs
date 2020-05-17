8c72dc32812ca4e5a4ab3cb040621e645507f0ab
Mathias Cordova <macordova@miuandes.cl>
2020-05-17 13:17:58 -0400

    Parte 1

---
+++ lab-6-mathiascordova/lab-6-mathiascordova/Empresa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab_6_mathiascordova
+{
+    [Serializable]
+    class Empresa
+    {
+        private string companyName;
+        private string companyRUT;
+
+        public void SetName(string name)
+        {
+            companyName = name;
+        }
+        public void SetRUT(string rut)
+        {
+            companyRUT = rut;
+        }
+
+        public string GetName()
+        {
+            return companyName;
+        }
+
+        public string GetRUT()
+        {
+            return companyRUT;
+        }
+    }
+}
---
+++ lab-6-mathiascordova/lab-6-mathiascordova/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab_6_mathiascordova
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            void Save(Empresa company)
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("empresa.bin", FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, company);
+                stream.Close();
+            }
+
+            Empresa Load()
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("empresa.bin", FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+                Empresa company = (Empresa)formatter.Deserialize(stream);
+                stream.Close();
+                return company;
+            }
+
+            Empresa empresa = new Empresa();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("INFO EMPRESAS" + Environment.NewLine + Environment.NewLine + "[1] Crear empresa desde cero" + Environment.NewLine + "[2] Ver datos de empresa existente" + Environment.NewLine + "[X] Cerrar programa");
+                Console.Write("==>  ");
+                string choice = Console.ReadLine();
+
+                if (choice == "x" || choice == "X")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Hasta la vista baby");
+                    System.Threading.Thread.Sleep(1000);
+                    Save(empresa);
+                    break;
+                }
+                
+
+                else if (choice == "1")
+                {
+                    Console.Clear();
+                    Console.Write("Ingrese el nombre de la empresa: ");
+                    empresa.SetName(Console.ReadLine());
+                    Console.Write("Ingrese el RUT de la empresa: ");
+                    empresa.SetRUT(Console.ReadLine());
+                    Save(empresa);
+                    Console.WriteLine("Se creo la empresa con exito");
+                    System.Threading.Thread.Sleep(1000);
+                    continue;
+                }
+
+
+                else if (choice == "2")
+                {
+                    empresa = Load();
+                    Console.Clear();
+                    Console.WriteLine("Nombre de empresa: "+empresa.GetName());
+                    Console.WriteLine("RUT de empresa: "+empresa.GetRUT());
+                    System.Threading.Thread.Sleep(4000);
+                    continue;
+                }
+
+
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Input invalido");
+                    System.Threading.Thread.Sleep(1000);
+                    continue;
+
+                }
+            }
+
+        }
+    }
+}
